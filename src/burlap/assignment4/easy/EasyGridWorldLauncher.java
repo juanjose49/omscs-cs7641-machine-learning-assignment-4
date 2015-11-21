697549a02b333c86a90bec31519d4c186b5edc2c
Juan J. San Emeterio <juanjser@gmail.com>
2015-11-21 13:29:47 -0500

    MapPrinter Class was added to output grid world map and policy

--- src/burlap/assignment4/easy/EasyGridWorldLauncher.java
+++ src/burlap/assignment4/easy/EasyGridWorldLauncher.java
@@ -1,19 +1,16 @@
 package burlap.assignment4.easy;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import burlap.assignment4.util.BasicRewardFunction;
 import burlap.assignment4.util.BasicTerminalFunction;
+import burlap.assignment4.util.MapPrinter;
 import burlap.behavior.policy.Policy;
 import burlap.behavior.singleagent.EpisodeAnalysis;
-import burlap.behavior.singleagent.auxiliary.EpisodeSequenceVisualizer;
 import burlap.behavior.singleagent.learning.tdmethods.QLearning;
 import burlap.behavior.singleagent.planning.stochastic.policyiteration.PolicyIteration;
 import burlap.behavior.singleagent.planning.stochastic.valueiteration.ValueIteration;
-import burlap.behavior.valuefunction.ValueFunctionInitialization;
-import burlap.domain.singleagent.gridworld.GridWorldVisualizer;
 import burlap.oomdp.core.Domain;
 import burlap.oomdp.core.TerminalFunction;
 import burlap.oomdp.core.states.State;
@@ -22,13 +19,12 @@ import burlap.oomdp.singleagent.environment.SimulatedEnvironment;
 import burlap.oomdp.singleagent.explorer.VisualExplorer;
 import burlap.oomdp.statehashing.SimpleHashableStateFactory;
 import burlap.oomdp.visualizer.Visualizer;
-import burlap.tutorials.cpl.QLTutorial;
 
 public class EasyGridWorldLauncher {
 
 	private static boolean visualizeInitialGridWorld = false;
-	private static boolean visualizeValueIteration = false;
-	private static boolean visualizePolicyIteration = false;
+	private static boolean visualizeValueIteration = true;
+	private static boolean visualizePolicyIteration = true;
 	private static boolean visualizeQLearning = true;
 	
 
@@ -44,14 +40,19 @@ public class EasyGridWorldLauncher {
 
 		SimulatedEnvironment env = new SimulatedEnvironment(domain, rf, tf,
 				initialState);
-
+		//Print the map that is being analyzed
+		MapPrinter.printMap(gen.getMap());
+		
 		if (visualizeInitialGridWorld) {
 			visualizeInitialGridWorld(domain, gen, env);
-		}else if(visualizeValueIteration){
+		}
+		if(visualizeValueIteration){
 			runValueIteration(gen,domain,initialState, rf, tf);
-		}else if(visualizePolicyIteration){
+		}
+		if(visualizePolicyIteration){
 			runPolicyIteration(gen,domain,initialState, rf, tf);
-		}else if(visualizeQLearning){
+		}
+		if(visualizeQLearning){
 			runQLearning(gen,domain,initialState, rf, tf, env);
 
 		}
@@ -73,6 +74,7 @@ public class EasyGridWorldLauncher {
 
 	private static void runValueIteration(EasyGridWorld gen, Domain domain,
 			State initialState, RewardFunction rf, TerminalFunction tf) {
+		System.out.println("//Value Iteration Analysis//");
 
 		ValueIteration vi = new ValueIteration(
 				domain,
@@ -80,20 +82,25 @@ public class EasyGridWorldLauncher {
 				tf,
 				0.99,
 				new SimpleHashableStateFactory(),
-				.5, 30);
+				.5, 10000000);
 
 		// run planning from our initial state
 		Policy p = vi.planFromState(initialState);
+		MapPrinter.printPolicyMap(vi.getAllStates(), p, gen);
 
 		// evaluate the policy with one roll out visualize the trajectory
 		EpisodeAnalysis ea = p.evaluateBehavior(initialState, rf, tf);
 
-		Visualizer v = gen.getVisualizer();
-		new EpisodeSequenceVisualizer(v, domain, Arrays.asList(ea));
+//		Visualizer v = gen.getVisualizer();
+//		new EpisodeSequenceVisualizer(v, domain, Arrays.asList(ea));
+		System.out.println("\n\n");
 	}
 	
+	
+
 	private static void runPolicyIteration(EasyGridWorld gen, Domain domain,
 			State initialState, RewardFunction rf, TerminalFunction tf) {
+		System.out.println("//Policy Iteration Analysis//");
 
 		PolicyIteration pi = new PolicyIteration(
 				domain,
@@ -105,22 +112,29 @@ public class EasyGridWorldLauncher {
 
 		// run planning from our initial state
 		Policy p = pi.planFromState(initialState);
+		MapPrinter.printPolicyMap(pi.getAllStates(), p, gen);
 
 		// evaluate the policy with one roll out visualize the trajectory
 		EpisodeAnalysis ea = p.evaluateBehavior(initialState, rf, tf);
 
-		Visualizer v = gen.getVisualizer();
-		new EpisodeSequenceVisualizer(v, domain, Arrays.asList(ea));
+//		Visualizer v = gen.getVisualizer();
+//		new EpisodeSequenceVisualizer(v, domain, Arrays.asList(ea));
+		System.out.println("\n\n");
+
 	}
 	
 	private static void runQLearning(EasyGridWorld gen, Domain domain,
 			State initialState, RewardFunction rf, TerminalFunction tf,SimulatedEnvironment env) {
+		System.out.println("//Q Learning Analysis//");
 
 		QLearning agent = new QLearning(
 				domain,
 				0.99,
 				new SimpleHashableStateFactory(),
-				0.1, 0.1);
+				0.99, 0.99);
+		agent.initializeForPlanning(rf, tf, 100000000);
+		Policy p =agent.planFromState(initialState);		
+		MapPrinter.printPolicyMap(getAllStates(domain,rf,tf,initialState), p, gen);
 
 		// run Q-learning and store results in a list
 		List<EpisodeAnalysis> episodes = new ArrayList<EpisodeAnalysis>(1000);
@@ -129,8 +143,24 @@ public class EasyGridWorldLauncher {
 			env.resetEnvironment();
 		}
 
-		Visualizer v = gen.getVisualizer();
-		new EpisodeSequenceVisualizer(v, domain, episodes);
+
+//		Visualizer v = gen.getVisualizer();
+//		new EpisodeSequenceVisualizer(v, domain, episodes);
 	}
 
+
+	
+	public static List<State> getAllStates(Domain domain,
+			 RewardFunction rf, TerminalFunction tf,State initialState){
+		ValueIteration vi = new ValueIteration(
+				domain,
+				rf,
+				tf,
+				0.99,
+				new SimpleHashableStateFactory(),
+				.5, 100);
+		Policy p = vi.planFromState(initialState);
+
+		return vi.getAllStates();
+	}
 }
---
+++ src/burlap/assignment4/util/MapPrinter.java
@@ -0,0 +1,93 @@
+package burlap.assignment4.util;
+
+import java.util.List;
+
+import burlap.assignment4.easy.EasyGridWorld;
+import burlap.behavior.policy.GreedyQPolicy;
+import burlap.behavior.policy.Policy;
+import burlap.oomdp.core.objects.ObjectInstance;
+import burlap.oomdp.core.states.State;
+import burlap.oomdp.core.values.Value;
+
+public class MapPrinter {
+
+	public static void printMap(int[][] map){
+		System.out.println("/////Grid World Analysis/////\n");
+		System.out.println("This is your grid world:");
+		for (int j = 0; j<map[0].length; j++){
+	    	 System.out.print("[");
+		     for (int i = 0; i<map.length; i++){
+		    	 System.out.print(String.valueOf(map[j][i]));
+		    	 if(i != map.length-1){
+		    		 System.out.print(",");
+		    	 }
+		     }
+	    	 System.out.print("]");
+
+	    	 System.out.println();
+		}
+   	 System.out.println("\n\n");
+
+	}
+	
+	public static void printPolicyMap(List<State> states, Policy p,EasyGridWorld gen) {
+		int[][] map = gen.getMap();
+
+		     
+		System.out.println();
+		
+		String[][] policy = new String[map[0].length][map.length];
+		
+		for(State state : states){
+			ObjectInstance agent = state.getObject("agent0");
+			List<Value> values = agent.getValues();
+			Value xValue = values.get(0);
+			int x = xValue.getDiscVal();
+			Value yValue = values.get(1);
+			int y = yValue.getDiscVal();
+			
+			String action;
+			if(p instanceof GreedyQPolicy){
+				action = p.getAction(state).toString();
+			}else{
+			action = p.getAction(state).toString();			
+			}
+			switch(action){
+			case "east": 
+				action = ">";
+				break;
+			case "north":
+				action = "^";
+				break;
+			case "west":
+				action = "<";
+				break;
+			case "south":
+				action = "v";
+				break;
+			default:
+				action = "*";
+			}
+			policy[x][y] = action;
+
+		}
+		
+		for (int j = 0; j<policy[0].length; j++){
+	    	 System.out.print("[");
+		     for (int i = 0; i<policy.length; i++){
+		    	 String action = String.valueOf(policy[j][i]);
+		    	 if(action.equals("null")) action ="*";
+		    	 System.out.print(action);
+		    	 if(i != policy.length-1){
+		    		 System.out.print(",");
+		    	 }
+		     }
+	    	 System.out.print("]");
+
+	    	 System.out.println();
+		}
+//		for(State state : vi.getAllStates()){
+//			System.out.println(state.toString()+":"+p.getAction(state).toString());
+//		}
+	}
+}
